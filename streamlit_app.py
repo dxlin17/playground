060c1a52a9736bcb501269349211a6fb4db46260
David Lin <davxlin@gmail.com>
2023-06-04 14:49:05 -0700

    Add requirements.txt and fix error

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,5 @@ st.subheader("Most common color")
 img_file = st.file_uploader("Choose a file")
 if img_file is not None:
     print(img_file.id, img_file.name, img_file.type, img_file.size)
-    print(uploaded_file.getvalue().shape)
-st.write("success!")
-    
+    print(img_file.getvalue().shape)
+ 
